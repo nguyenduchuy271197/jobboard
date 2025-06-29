1bc2ab2e0939df389816d0c9177b5b21bc0ffce0
Huy Nguyen <nguyenduchuy27111997@gmail.com>
2025-06-29 19:01:27 +0700

    chore

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 import { BriefcaseIcon } from "lucide-react";
 import Link from "next/link";
 
-export default function Logo({ onlyIcon = false }: { onlyIcon: boolean }) {
+export default function Logo({ onlyIcon = false }: { onlyIcon?: boolean }) {
   return (
     <Link href="/" className="flex items-center space-x-2">
       <BriefcaseIcon className="h-6 w-6 text-primary" />
